ad0d999540021a4754aaeff2a9abd1636464f63d
AmirReza Esfandiyari <ecourapp@gmail.com>
2026-06-13 18:03:54 +0330

    change netifaces

--- get_local_ip.py
+++ get_local_ip.py
@@ -1,13 +1,16 @@
-import netifaces
+import socket
 
 possibles = ['172', '192', '127']
 ports = [9877, 9922, 5041, 2982, 7309]
 
 def get_private_ip_and_subnet():
-    default_gws = netifaces.gateways().get('default', {})
-    for interface in netifaces.interfaces():
-        for addr in netifaces.ifaddresses(interface).get(netifaces.AF_INET, []):
-            if (ip := addr['addr']).split('.')[0] in possibles:
-                if any(gw[1] == interface for gw in default_gws.values()):
-                    return ip, addr['netmask']
+    try:
+        # Connect to an external address to determine the default route interface IP
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))  # Doesn't actually send data
+            ip = s.getsockname()[0]
+        if ip.split('.')[0] in possibles:
+            return ip, None  # socket alone can't retrieve the subnet mask
+    except OSError:
+        pass
     return None, None
